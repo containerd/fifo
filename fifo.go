e5b3831a4a55d4e0b63b18dcce8d3d6d10948911
ningmingxiao <ning.mingxiao@zte.com.cn>
2023-04-24 16:31:58 +0800

    add check file type is fifo
    Signed-off-by: ningmingxiao <ning.mingxiao@zte.com.cn>

--- fifo.go
+++ fifo.go
@@ -80,13 +80,17 @@ func OpenFifo(ctx context.Context, fn string, flag int, perm os.FileMode) (io.Re
 }
 
 func openFifo(ctx context.Context, fn string, flag int, perm os.FileMode) (*fifo, error) {
-	if _, err := os.Stat(fn); err != nil {
+	if file, err := os.Stat(fn); err != nil {
 		if os.IsNotExist(err) && flag&syscall.O_CREAT != 0 {
 			if err := syscall.Mkfifo(fn, uint32(perm&os.ModePerm)); err != nil && !os.IsExist(err) {
 				return nil, fmt.Errorf("error creating fifo %v: %w", fn, err)
 			}
 		} else {
 			return nil, err
+		}
+	} else {
+		if file.Mode()&os.ModeNamedPipe == 0 {
+			return nil, fmt.Errorf("file %s is not fifo", fn)
 		}
 	}
 
